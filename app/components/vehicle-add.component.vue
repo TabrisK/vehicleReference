33100d4aa3e8404dac4c9430a1ebcce33622b117
TabrisK <601329930@qq.com>
2017-02-13 18:21:45 +0800

    update form.directive

--- app/components/vehicle-add.component.vue
+++ app/components/vehicle-add.component.vue
@@ -1,11 +1,26 @@
 <template>
     <div>
         <form @submit.prevent="addVehicle(vehicle)" v-form="addVForm">
-            <div><input v-model="vehicle.plate" placeholder="车牌" maxlength="7"></div>
-            <div><input v-model="vehicle.brand" placeholder="品牌"></div>
-            <div><input v-model="vehicle.contact" placeholder="联系人"></div>
-            <div><input v-model.number="vehicle.phone" type="number" placeholder="联系方式"></div>
-            <button type="submit">提交</button>
+            <input name="plate" v-model="vehicle.plate" placeholder="车牌"
+                   pattern="^[\u4e00-\u9fa5]{1}[A-Z0-9]*$" maxlength="7" required
+                   :class="{'error': addVForm.plate?addVForm.plate.$dirty && addVForm.plate.$invalid:false}">
+            <input name="brand" v-model="vehicle.brand" placeholder="品牌"
+                   :class="{'error': addVForm.brand?addVForm.brand.$dirty && addVForm.brand.$invalid:false}">
+            <input name="contact" v-model="vehicle.contact" placeholder="联系人" required
+                   :class="{'error': addVForm.contact?addVForm.contact.$dirty && addVForm.contact.$invalid:false}">
+            <input name="phone" v-model.number="vehicle.phone" type="number" placeholder="联系方式" required
+                   :class="{'error': addVForm.phone?addVForm.phone.$dirty && addVForm.phone.$invalid:false}">
+            <select name="device" required
+                    :class="{'error': addVForm.device?addVForm.device.$dirty && addVForm.device.$invalid:false}">
+                <option disabled>选择设备类型</option>
+                <option value="1">内部设备</option>
+                <option value="2">外部设备</option>
+            </select>
+            <button :disabled="addVForm.$invalid" type="submit"
+                    :class="{'disabled': addVForm.$invalid}"
+            >提交
+            </button>
+            {{addVForm}}
         </form>
     </div>
 </template>
@@ -36,18 +51,23 @@
 </script>
 <style lang="scss" rel="stylesheet/scss">
     form {
-        div {
+        input {
+            border-left: 0px;
+            border-right: 0px;
             width: 100%;
-            input {
-                border-left: 0px;
-                border-right: 0px;
-                width: 100%;
-                line-height: 26px;
-                font-size: 18px;
-                padding: 0px 10px;
-                margin: 10px 0px;
-                display: table-cell;
-            }
+            line-height: 26px;
+            font-size: 18px;
+            padding: 0px 10px;
+            margin: 10px 0px;
+            box-sizing: border-box;
         }
+    }
+
+    .error {
+        border: 2px solid red;
+    }
+
+    .disabled {
+        color: gray;
     }
 </style>
--- app/directives/form/form.directive.js
+++ app/directives/form/form.directive.js
@@ -1,20 +1,101 @@
 /**
  * Created by Helex on 2017/2/12.
  */
+/*
+ ValidityState Reference: https://developer.mozilla.org/en-US/docs/Web/API/ValidityState
+ var errorMsg = {
+ //Is a Boolean indicating the user has provided input that the browser is unable to convert.
+ badInput: false,
+ //Is a Boolean indicating the element's custom validity message has been set to a non-empty string
+ //by calling the element's setCustomValidity() method.
+ customError: false,
+ //Is a Boolean indicating the value does not match the specified pattern.
+ patternMismatch: false,
+ //Is a Boolean indicating the value is greater than the maximum specified by the max attribute.
+ rangeOverflow: false,
+ //Is a Boolean indicating the value is less than the minimum specified by the min attribute.
+ rangeUnderflow: false,
+ //Is a Boolean indicating the value does not fit the rules determined by the step attribute
+ //(that is, it's not evenly divisible by the step value).
+ stepMismatch: false,
+ //Is a Boolean indicating the value exceeds the specified maxlength for HTMLInputElement or HTMLTextAreaElement objects.
+ //Note: This will never be true in Gecko, because elements' values are prevented from being longer than maxlength.
+ tooLong: false,
+ //Is a Boolean indicating the value fails to meet the specified minlength for HTMLInputElement or HTMLTextAreaElement objects.
+ tooShort: false,
+ //Is a Boolean indicating the value is not in the required syntax (when type is email or url).
+ typeMismatch: false,
+ //Is a Boolean indicating the element meets all constraint validations, and is therefore considered to be valid.
+ valid: true,
+ //Is a Boolean indicating the element has a required attribute, but no value.
+ valueMissing: false
+ }*/
+
+//正则表达式
+let reg = {};
 
 export default {
-    install: function(Vue, options){
-        Vue.directive("form",{
-            bind: function(el, binding, vnode){
-                console.log(binding.value);
+    install: function (Vue, options) {
+        Vue.directive("form", {
+            bind: function (el, binding, vnode) {
             },
-            inserted: function(el, binding, vnode){
+            inserted: function (el, binding, vnode) {
+                var vm = vnode.context,//rendered in this component's scope
+                    formObj = {},//用户表单对象；
+                    formNode = vnode.elm;//表单virtual dom
+                for (let node of vnode.elm) {
+                    //匹配到需要监控的输入控件
+                    if (node.nodeName == "INPUT") {
+                        updateStatusListerner(node);
+                        node.addEventListener("keyup", (e)=> {
+                            updateStatusListerner(node, e)
+                        });
 
+                    } else if (node.nodeName == "SELECT") {
+                        updateStatusListerner(node);
+                        node.addEventListener("change", (e)=> {
+                            updateStatusListerner(node, e)
+                        });
+                    }
+                }
+
+                /**
+                 *
+                 * @param isValid   form是否通过验证
+                 * @param msg       用户本次操作验证元素信息
+                 */
+                function setValidation(isValid, msg, e) {//设置用户表单对象合法性
+                    "use strict";
+                    formObj.$valid = isValid;
+                    formObj.$invalid = !isValid;
+                    formObj.$dirty = !!e;
+                    setErrorMsg(formObj, msg, !!e);
+
+                    console.log(formObj);
+                    vm.$data[binding.expression] = Object.assign({}, vm.$data[binding.expression], formObj);
+
+                    function setErrorMsg(err, msg, userOperation) {
+                        err[msg.name] = {$dirty: userOperation};
+                        err[msg.name].$valid = msg.validation.valid;
+                        err[msg.name].$invalid = !msg.validation.valid;
+                        for (let key in msg.validation) {
+                            if (msg.validation[key] && key != "valid")
+                                err[msg.name][key] = msg.validation[key];
+                        }
+                    }
+                }
+
+                function updateStatusListerner(node, e) {//监听更新用户表单状态方法
+                    "use strict";
+                    setValidation(formNode.checkValidity(), {
+                        name: node.name,//元素名
+                        validation: node.validity//校验状态
+                    }, e);
+                }
             },
-            update: function(el, binding, vnode, oldVnode){
+            update: function (el, binding, vnode, oldVnode) {
             },
-            componentUpdated: function(el, binding, vnode, oldVnode){
-                console.log(vnode);
+            componentUpdated: function (el, binding, vnode, oldVnode) {
             }
         });
     }
